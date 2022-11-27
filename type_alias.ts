95ff99de6170459a098275fc889821c2e62fa902
Phichet Parto <64160261@go.buu.ac.th>
2022-11-27 21:01:18 +0700

    Type Alias

---
+++ type_alias.ts
@@ -0,0 +1,20 @@
+type CarYear = number;
+type CarType = string;
+type CarModel = string;
+
+type Car = {
+    year: CarYear,
+    type: CarType,
+    model: CarModel
+}
+
+const carYear: CarYear = 2001;
+const carType: CarType = "Toyota";
+const carModel: CarModel = "Corolla";
+
+const car1: Car = {
+    year: 2001,
+    type: "Nissan",
+    model: "XXX",
+}
+console.log(car1)
